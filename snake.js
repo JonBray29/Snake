f01fb270f23629cb645721f5ccd49c9ddf004939
Jonathon Bray <jonathonbray@Jonathons-MacBook-Pro.local>
2020-11-13 11:45:06 +0000

    Completed

--- snake.js
+++ snake.js
@@ -1,9 +1,11 @@
 $(function(){
     var canvasContext = $("#snakeCanvas")[0].getContext("2d");
-    var hello = "hello";
     var snake = [ {x: 250, y: 250}, {x: 260, y: 250}, {x: 270, y: 250}, ];
     var dx = -10;
     var dy = 0;
+    var foodX;
+    var foodY;
+    var score = 0;
     var changingDirection = false;
 
     $(document).keydown(function(event){
@@ -11,6 +13,7 @@ $(function(){
     });
 
     drawCanvas();
+    generateFood();
 
     function drawCanvas(){
         if (gameEnded()) return;
@@ -19,6 +22,7 @@ $(function(){
 
         setTimeout(function onTick(){
             fillCanvas();
+            drawFood();
             moveSnake();
             snake.forEach(drawSnake);
             drawCanvas();
@@ -40,7 +44,17 @@ $(function(){
     function moveSnake(){
         var newHead = {x: snake[0].x + dx, y: snake[0].y + dy};
         snake.unshift(newHead);
-        snake.pop();
+
+        const hasEatenFood = snake[0].x === foodX && snake[0].y === foodY;
+
+        if(hasEatenFood){
+            score += 1;
+            $("#score").html(score);
+            generateFood();
+        }
+        else{
+            snake.pop();
+        }
     }
     //Change direction of snake
     function changeDirection(event){
@@ -72,17 +86,38 @@ $(function(){
     //Has game ender
     function gameEnded(){
         for(let i = 4; i < snake.length; i++){
-            var hasCollided = snake[i].x === snake[i].x && snake[i].y === snake[i].y;
+            var hasCollided = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
 
             if(hasCollided) return true;
         }
 
         var hitTopWall = snake[0].y < 0;
-        var hitBottomWall = snake[0].y > $("#snakeCanvas").height - 10;
-        var hitRightWall = snake[0].x > $("snakeCanvas").width - 10;
+        var hitBottomWall = snake[0].y > $("#snakeCanvas")[0].height - 10;
+        var hitRightWall = snake[0].x > $("#snakeCanvas")[0].width - 10;
         var hitLeftWall = snake[0].x < 0;
 
         return hitTopWall || hitBottomWall || hitRightWall || hitLeftWall;
     }
+    //Get random coordinate for the food
+    function randomFood(min, max){
+        return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+    }
+    //Generate food location
+    function generateFood(){
+        foodX = randomFood(0, $("#snakeCanvas")[0].width - 10);
+        foodY = randomFood(0, $("#snakeCanvas")[0].height - 10);
+
+        snake.forEach(function hasEatenFood(part){
+            let hasEaten = part.x == foodX && part.y == foodY;
+            if(hasEaten) generateFood();
+        });
+    }
+    //Draw food onto the canvas
+    function drawFood(){
+        canvasContext.fillStyle = "white";
+        canvasContext.strokeStyle = "white";
+        canvasContext.fillRect(foodX, foodY, 10, 10);
+        canvasContext.strokeRect(foodX, foodY, 10, 10);
+    }         
 })
 
